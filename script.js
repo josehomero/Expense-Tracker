f5ffddbff4784d4abbd3a8aa1f331cf2557f113a
josehomero <jose.homero.rios@outlook.com>
2020-02-08 19:07:47 -0800

    updated

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-document.console.log("hello world");
+
+var trItems = document.getElementById('tabel-row-items');
+const button = document.getElementById('button');
+const form = document.querySelector('form');
+let whenInput = form.elements.when;
+
+
+button.addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log(form.whenInput.value);
+    //I was trying to grab the form and when input on the line above this one
+});
+
+
+const createTrElement = function (text) {
+    var expenseTd = document.createElement('td');
+    expenseTd.textContent = text;
+    trItems.appendChild(expenseTd);
+};
