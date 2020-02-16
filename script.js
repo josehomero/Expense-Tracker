f5010d1fbe789ae09f224531451c5a4aeaeafc2a
josehomero <jose.homero.rios@outlook.com>
2020-02-16 15:22:19 -0800

    update

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
 const form = document.querySelector('form');
 
+//The buuton from the html is called
 const button = document.getElementById('button');
+//button is applied to eventListener
 button.addEventListener('click', function(e) {
     e.preventDefault();
+    //variables are made to target the different inputs in the html bt their names
     var typeInput = form.type.value;
     var whenInput = form.when.value;
     var whereInput = form.where.value;
     var costInput = form.cost.value;
-
-    createTrElement(typeInput,whenInput,whereInput,costInput);
-
+    //createTrElement is applied to the different variables
+    createTdElement(typeInput,whenInput,whereInput,costInput);
+    //a string value is given to the variables to allow the inserted data to pass
     form.type.value = '';
     form.when.value = '';
     form.where.value = '';
     form.cost.value = '';
 });
 
-//button.addEventListener('click', function(e) {
-  //  var expenseTr = document.createElement('tr');
-    //tableBody.appendChild(expenseTr);
-//})
-
 
 var trItems = document.getElementById('table-row-items');
 const tableBody = document.getElementById('table-body');
 
 const table = document.getElementById('my-table');
-//var row = table.insertRow(-1);
-//console.log(row);
-//row.insertCell(0).textContent = "new data";
 
-console.log(tableBody);
+const createTdElement = function(text) {
+    var tR = document.createElement('tr');
+    tableBody.appendChild(tR);
 
-const createTrElement = function(type,when,where,cost) {
-    console.log("Type: ", type);
-    console.log("When: ", when);
-    console.log("Where: ", where);
-    console.log("Cost: ", cost);
+    var typeTd = document.createElement('td');
+    typeTd.textContent = text;
+    tR.appendChild(typeTd);
+
+    var whenTd = document.createElement('td');
+    whenTd.textContent = text;
+    tR.appendChild(whenTd);
+
+    var whereTd = document.createElement('td');
+    whereTd.textContent = text;
+    tR.appendChild(whereTd);
+
+    var costTd = document.createElement('td');
+    costTd.textContent = text;
+    tR.appendChild(costTd);
+
 };
+
